a4147b84279993ac175baa2d596780db1adee84c
Nupoor Nitin Kotasthane <nupoor292k@gmail.com>
2015-12-06 14:58:30 -0800

    Changed the getSkiRecords API

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ var eskimoAPP = function() {
         self.app.post('/attendEvent', UserAPIs.attendEvent);
         self.app.delete('/deleteEvent', UserAPIs.deleteEvent);
         self.app.post('/createSkiRecord', SkiAPIs.createSkiRecord);
-        self.app.post('/getSkiRecords', SkiAPIs.getSkiRecords);
+        self.app.get('/getSkiRecords:user_id', SkiAPIs.getSkiRecords);
         self.app.delete('/deleteSkiRecord', SkiAPIs.deleteSkiRecord);
         self.app.get('/getAttendingEvents/:user_id', UserAPIs.getAttendingEvents); 
         self.app.delete('/deleteEventRecord', eventAPIs.deleteEventRecord);
